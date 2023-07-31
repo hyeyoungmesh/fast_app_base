57b549bbdaa658ca179a3601ea0af1bcbe02f7da
hyeyoung.moon <hyeyoung.moon@meshkorea.net>
2023-08-01 07:10:30 +0900

    feat : 알림 화면

--- lib/screen/main/tab/home/w_ttoss_app_bar.dart
+++ lib/screen/main/tab/home/w_ttoss_app_bar.dart
@@ -3,7 +3,9 @@ import 'package:fast_app_base/common/dart/extension/context_extension.dart';
 import 'package:fast_app_base/common/widget/w_empty_expanded.dart';
 import 'package:fast_app_base/common/widget/w_height_and_width.dart';
 import 'package:fast_app_base/common/widget/w_tap.dart';
+import 'package:fast_app_base/screen/notification/s_notification.dart';
 import 'package:flutter/material.dart';
+import 'package:nav/nav.dart';
 
 class TtossAppBar extends StatefulWidget {
   static const double appBarHeight = 60;
@@ -33,9 +35,7 @@ class _TtossAppBarState extends State<TtossAppBar> {
           width10,
           Tap(
             onTap: () {
-              setState(() {
-                _showRedDot = !_showRedDot;
-              });
+              Nav.push(NotificationScreen());
             },
             child: Stack(
               children: [
---
+++ lib/screen/notification/d_notification.dart
@@ -0,0 +1,41 @@
+import 'package:fast_app_base/screen/notification/vo/vo_notification.dart';
+import 'package:fast_app_base/screen/notification/w_notification_item.dart';
+import 'package:flutter/material.dart';
+import 'package:nav/dialog/dialog.dart';
+import 'package:nav/enum/enum_nav_ani.dart';
+
+class NotificationDialog extends DialogWidget {
+  final List<TtossNotification> notifications;
+  NotificationDialog(
+    this.notifications, {
+    Key? key,
+  }) : super(
+          key: key,
+          animation: NavAni.Bottom,
+          barrierDismissible: false,
+        );
+
+  @override
+  State<NotificationDialog> createState() => _NotificationDialogState();
+}
+
+class _NotificationDialogState extends State<NotificationDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      type: MaterialType.transparency,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          ...widget.notifications
+              .map((element) => NotificationItemWidget(
+                  notification: element,
+                  onTap: () {
+                    widget.hide(); //다이얼로그는 위젯 하이드 써주기
+                  }))
+              .toList()
+        ],
+      ),
+    );
+  }
+}
